2d6c1f4b20b251a955cabf2673f2392852bd9181
Viktoriiapon <viktoriyaponom@gmail.com>
2024-06-04 07:29:50 +0200

    change reguired email

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -6,7 +6,7 @@ const contactSchema = new Schema(
   {
     name: { type: String, required: true },
     phoneNumber: { type: String, required: true },
-    email: { type: String, required: true },
+    email: { type: String, required: false },
     isFavourite: { type: Boolean, default: false },
     contactType: {
       type: String,
