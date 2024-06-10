a577161fca40b5b2eae77cccf97a77abd4fa254c
Viktoriiapon <viktoriyaponom@gmail.com>
2024-06-10 18:53:59 +0200

    change validate post

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -4,7 +4,7 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
-  email: Joi.string().email().min(3).max(20).required(),
+  email: Joi.string().email().min(3).max(20),
   phoneNumber: Joi.string().min(3).max(20).required(),
   isFavourite: Joi.boolean(),
   contactType: Joi.string().min(3).max(20).valid('work', 'home', 'personal'),
